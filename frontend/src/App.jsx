52b861d6661ca2f63005faa02ff87e7ab4dc5a4a
Likon Barman <mrlikhonbarman@gmail.com>
2025-11-15 23:18:57 +0530

    add mockdata

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,12 @@ import Footer from './components/Footer';
 import SubNavbar from './components/SubNavbar';
 
 // CORRECT PATHS
-import Home from './pages/home/Home';
+import Home from './pages/home/HomePage';
 // import Products from './pages/products/Products';
 import Supplier from './pages/supplier/Supplier';
 import Login from './pages/login/Login';
 import Shops from './pages/shops/Shops';
+import HomePage from './pages/home/HomePage';
 
 function App() {
   return (
@@ -18,7 +19,7 @@ function App() {
       <SubNavbar/>
       <div style={{ padding: '2rem' }}>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<HomePage />} />
           
           <Route path="/shops" element={<Shops />} />
           <Route path="/supplier" element={<Supplier />} />
---
+++ frontend/src/data/mockdata.js
@@ -0,0 +1,11 @@
+// mockData.js
+export const localProducts = [
+  { id: 1, name: "Fashions", image: "../../public/images/cloths.jpg", Offer: "Offer : 90%" },
+  { id: 2, name: "Electronics", image: "../../public/images/electronics.jpg", Offer: "Offer : 90%" },
+  { id: 3, name: "Local Foods", image: "../../public/images/food.jpg",  Offer: "Offer : 90%" },
+  { id: 3, name: "Fruits", image: "../../public/images/mango.jpg",  Offer: "Offer : 90%" },
+  { id: 3, name: "Milk & Honey", image: "../../public/images/milk.jpg",  Offer: "Offer : 90%" },
+  { id: 3, name: "Rice", image: "../../public/images/rice.jpg",  Offer: "Offer : 90%" },
+  { id: 3, name: "Fartilizers", image: "../../public/images/fartilzer.jpg",  Offer: "Offer : 90%" }
+  // Add more products as needed
+];
